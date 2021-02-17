04b3c88bb55369fa7ed09b3068ec587ba6c2ec54
addy100 <72308696+addy100@users.noreply.github.com>
2021-02-17 23:58:06 +0530

    added atm_machine

---
+++ atm_machine.py
@@ -0,0 +1,12 @@
+## atm machine
+input ("enter your Account Number")
+print ("Current Account Balance: 25000")
+
+str choice = input("press 1 to make a Deposit or 2 to make a  Withdarawal")
+
+if choice == 1
+    str deposit = input("Press 1 to deposit Current Account, 2 for Savings Account")
+if deposit == 1
+    print("please insert check or money now")
+elif deposit == 2
+    print("please insert cheque or cash")
